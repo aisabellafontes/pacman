d7ac2f6f93a7a0f0b8985aa7be9b1d07cbbccfba
Isabella Santos <isabella.santos@mstech.com.br>
2015-08-20 00:57:34 -0300

    Reestruturaca da base do Jogo.

---
+++ Background.java
@@ -0,0 +1,20 @@
+/* Drew Schuster */
+import java.awt.*;
+import javax.imageio.*;
+import javax.swing.JPanel;
+import java.lang.Math;
+import java.util.*;
+import java.io.*;
+
+/*This board class contains the player, ghosts, pellets, and most of the game logic.*/
+public class Background extends JPanel{
+
+  Image pacmanClientAImage = Toolkit.getDefaultToolkit().getImage("images/pacmanA.jpg");
+  Image pacmanClientBImage = Toolkit.getDefaultToolkit().getImage("images/pacmanB.jpg");
+  Image pacmanClientBImage = Toolkit.getDefaultToolkit().getImage("images/pacmanB.jpg");
+
+  Image titleScreenImage = Toolkit.getDefaultToolkit().getImage("img/titleScreen.jpg");
+  Image gameOverImage = Toolkit.getDefaultToolkit().getImage("img/gameOver.jpg");
+  Image winScreenImage = Toolkit.getDefaultToolkit().getImage("img/winScreen.jpg");
+
+}
--- Pacman.java
+++ Pacman.java
@@ -1,103 +1,32 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.net.URL;
+/* Drew Schuster */
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.geom.Point2D;
+import javax.swing.JApplet;
+import java.awt.*;
+import java.util.*;
+import java.lang.*;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+public class Pacman extends JApplet implements MouseListener, KeyListener{
 
-import java.io.*;
-import javax.imageio.*;
+  public Pacman()
+  {
+    b.requestFocus();
 
-public class Pacman extends JFrame {
-    public Pacman() {
-        this.setTitle("Pacman Flavinha e Isa");
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(new Dimension(500, 500));
-        this.setLocationRelativeTo(null);
-        this.getContentPane().setLayout(new BorderLayout());
-        this.getContentPane().add(new GamePanel());
-        this.setVisible(true);
-    }
+    /* Create and set up window frame*/
+    JFrame f=new JFrame();
+    f.setSize(800,600);
 
-    public static void main(String[] args) {
-        new Pacman();
-    }
+    /* Add the board to the frame */
+    f.add(b,BorderLayout.CENTER);
 
-    public class GamePanel extends JPanel {
-        private Image pmImage;
-        private int xCoordinate = 200;
-        private int yCoordinate = 200;
-        boolean key_right, key_left, key_down, key_up; // Input booleans
+    /*Set listeners for mouse actions and button clicks*/
+    b.addMouseListener(this);
+    b.addKeyListener(this);
 
-        URL urlForImage;
-        ImageIcon usFlag;
-        Image avatar[] = new Image[6]; //para o boneco.gif
+    /* Make frame visible, disable resizing */
+    f.setVisible(true);
+    f.setResizable(false);
+}
 
-        public GamePanel() {
-            loadImage("C");
-            this.setFocusable(true);
-            addKeyListener(new GameInput()); // Add it to the JPanel
-        }
-
-        public void loadImage(String img_type) {
-
-          switch(img_type){
-
-            case "D":
-              avatar[0] = ImageIO.read(new File("anda0.gif"));
-              break;
-            case "U":
-              avatar[0] = ImageIO.read(new File("anda0.gif"));
-              break;
-            case "R": break;
-            case "L": break;
-            case "C": break;
-            //avatar[PARADO] = ImageIO.read(new File("parado.gif"));
-            avatar[ANDA0] = ImageIO.read(new File("anda0.gif"));
-            avatar[ANDA1] = ImageIO.read(new File("anda1.gif"));
-            avatar[SOCO0] = ImageIO.read(new File("soco0.gif"));
-            avatar[SOCO1] = ImageIO.read(new File("soco1.gif"));
-            avatar[SOCO2] = ImageIO.read(new File("soco2.gif"));
-          }
-
-        }
-
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            setBackground(Color.white);
-            g.drawImage(pmImage, xCoordinate, yCoordinate, this);
-
-            if (key_down) { loadImage("D"); yCoordinate++; }
-            if (key_up) { loadImage("U"); yCoordinate--; }
-            if (key_right) { loadImage("R"); xCoordinate++; }
-            if (key_left) { loadImage("L"); xCoordinate--; }
-            //for (int index = 0; index < 10000000; index++) {}
-
-            repaint();
-        }
-
-        private class GameInput implements KeyListener {
-            public void keyTyped(KeyEvent e) {}
-
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == e.VK_DOWN) key_down = false;
-                if (e.getKeyCode() == e.VK_UP) key_up = false;
-                if (e.getKeyCode() == e.VK_RIGHT) key_right = false;
-                if (e.getKeyCode() == e.VK_LEFT) key_left = false;
-            }
-
-            public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode() == e.VK_DOWN) key_down = true;
-                if (e.getKeyCode() == e.VK_UP) key_up = true;
-                if (e.getKeyCode() == e.VK_RIGHT) key_right = true;
-                if (e.getKeyCode() == e.VK_LEFT) key_left = true;
-            }
-        }
-    }
 }
